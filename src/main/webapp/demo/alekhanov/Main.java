dc0e11af39463372d3f457ed7e80cba053a78587
VAlekhanov <bitvull@gmail.com>
2022-01-25 19:32:28 +0400

    Echo bot on java

---
+++ src/main/webapp/demo/alekhanov/Main.java
@@ -0,0 +1,18 @@
+package alekhanov;
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class Main {
+    public static void main(String[] args) throws TelegramApiException {
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new Bot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+    public Main() throws TelegramApiException {
+    }
+}
